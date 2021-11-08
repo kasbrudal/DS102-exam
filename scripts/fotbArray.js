8728e2b0dad2dccffaa9c49a11bfda73c425d895
SourMaoam <julianhenriksen@hotmail.com>
2021-11-08 14:31:57 +0100

    footballArray

--- scripts/fotbArray.js
+++ scripts/fotbArray.js
@@ -5,10 +5,124 @@ var fotbArray = [
        position:"Right wing",
        team:"PSG",
        age: 34,
-       playerInfo:"",
-       playerImg
+       playerInfo:"hei",
+       playerImg:"images/messi.png",
 
 
-    }
+    },
+
+    {
+        player:"N'Golo Kanté",
+        nation: "France",
+        ranking:11,
+        position:"Central midfielder",
+        team:"Chelsea",
+        age: 30,
+        playerInfo:" A dynamic, diminutive, and disciplined player,[92][93][94] Kanté is known for his relentless energy and excellent ball-winning abilities as a box-to-box midfielder in the centre of the pitch.",
+        playerImg:"images/kante.png",
+    },
+
+    {
+        player:"Thomas Muller",
+        nation: "Germany",
+        ranking:12,
+        position:"Forward",
+        team:"Bayern Munich",
+        age: 32,
+        playerInfo:" Müller is widely regarded as one of the best off-the-ball players of all time due to his immaculate positional awareness.",
+        playerImg:"images/muller.png",
+    },
+
+    {
+        player:"Jan Oblak",
+        nation:"Slovenia",
+        ranking:13,
+        position:"Goalkeeper",
+        team:"Atlético Madrid",
+        age: 28,
+        playerInfo:" In 2014 Oblak moved to Atlético Madrid for a fee of €16 million, becoming La Liga's most expensive goalkeeper at the time.",
+        playerImg:"images/oblak.png"
+    },
+
+    {
+        player:"Joshua Kimmich",
+        nation: "Germany",
+        ranking: 14,
+        position:"Midfielder",
+        team:"Bayern Munich",
+        age: 26,
+        playerInfo:"Kimmich is known for his versatility, and has been referred to as a `Swiss Army knife` of a player",
+        playerImg:"images/kimmich.png",
+    },
+
+    {
+        player:"Mohamed Salah",
+        nation: "Egypt",
+        ranking: 15,
+        position:"Forward",
+        team:"Liverpool",
+        age: 29,
+        playerInfo:"Considered one of the best players in the world, Salah is known for his finishing, dribbling, and speed.",
+        playerImg:"images/salah.png",
+    },
+
+    {
+        player:"Karim Benzema",
+        nation: "France",
+        ranking: 16,
+        position:"Striker",
+        team:"Real Madrid",
+        age: 33,
+        playerInfo:"A creative forward known for his playmaking and finishing, Benzema is regarded as one of the best strikers in the world.",
+        playerImg:"images/benzema.png",
+    },
+
+    {
+        player:"Leon Goretzka",
+        nation: "Germany",
+        ranking: 17,
+        position:"Midfielder",
+        team:"Bayern Munich",
+        age: 26,
+        playerInfo:"At 1.89 m, Goretzka's high-jumping ability helps him to win aerial battles against strong and tall defenders.",
+        playerImg:"images/goretzka.png",
+    },
+
+    {
+        player:"Riyad Mahrez",
+        nation: "France",
+        ranking: 18,
+        position:"Winger",
+        team:"Manchester City",
+        age: 30,
+        playerInfo:"A quick, creative and technically gifted player, his main traits are his trickery, balance, turn of pace, flair and dribbling skills.",
+        playerImg:"images/mahrez.png",
+    },
+
+    {
+        player:"Ruben Dias",
+        nation: "Portugal",
+        ranking: 19,
+        position:"Centre-back",
+        team:"Manchester City",
+        age: 24,
+        playerInfo:"Combining his two-footedness with a very good passing range, Dias can dictate play in any direction from the back.",
+        playerImg:"images/dias.png",
+    },
+
+    {
+        player:"Jadon Sancho",
+        nation: "England",
+        ranking: 20,
+        position:"Winger",
+        team:"Manchester United",
+        age: 21,
+        playerInfo:"Regarded as a highly technical, creative player, Sancho is known for his trickery and use of feints in one-on-one situations and has been described as one of the world's best young players.",
+        playerImg:"images/sancho.png",
+    },
 ]
+
+const getAllPlayers = () =>{
+    return fotbArray;
+} 
 
