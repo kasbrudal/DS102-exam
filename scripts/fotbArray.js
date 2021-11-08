1e245074708def9f32b8411bdab02b9650861bb1
Kasper <peephyt@gmail.com>
2021-11-08 14:35:54 +0100

    s

--- scripts/fotbArray.js
+++ scripts/fotbArray.js
@@ -1,14 +1,103 @@
 var fotbArray = [
     {
        player:"Lionel Messi",
+       nation:"Argentina",
        ranking:1,
        position:"Right wing",
        team:"PSG",
        age: 34,
-       playerInfo:"",
-       playerImg
-
-
+       playerInfo:"Lionel Messi remains the best player in the world. Messi is known to be a Barcelona player, but he parted ways with them in the summer of 21 to join PSG. He scored 672 goals and 300 assists for Barcelona in 778 games",
+       playerImg:"images/messi.png"
+    },
+    {
+        player:"Robert Lewandowski",
+        nation:"Poland",
+        ranking:2,
+        position:"Striker",
+        team:"Bayern Munich",
+        age: 33,
+        playerInfo:"Lewandowski is a definition on a goalscorer, few players scores less goals than him. He was often overlooked and in the shadow of Messi and Ronaldo, but not anymore",
+        playerImg:"images/lewandowski.png"
+    },
+    {
+        player:"Kevin De Bruyne",
+        nation:"Belgium",
+        ranking:3,
+        position:"Central Attacking Midfield",
+        team:"Manchester City",
+        age: 30,
+        playerInfo:"Probably the most gifted techincally player. De Bruyne has eagle eyes and can send the most insane passes. His techincal ability and intelligence is important for Man city who have dominated the premier league 2 years in a row",
+        playerImg:"images/bruyne.png"
+    },
+    {
+        player:"Cristiano Ronaldo",
+        nation:"Portugal",
+        ranking:4,
+        position:"Striker",
+        team:"Manchester United",
+        age: 36,
+        playerInfo:"Cristiano Ronaldo or CR7 is one of the best players the world have seen. He is up there with the largest star. Lionel Messi biggest rival. CR7 recently joined backed Man Utd, the club that made him a big star but the results have been disappointing",
+        playerImg:"images/ronaldo.png"
+    },
+    {
+        player:"Harry Kane",
+        nation:"England",
+        ranking:5,
+        position:"Striker",
+        team:"Tottenham",
+        age: 28,
+        playerInfo:"Probably the most controvelsal ranking on this list, but Harry Kane is a lethal goalscorer. He scores left and right, but Tottenham have never won a trophy. Many consider him a top 10 player, but he has to approve that he can wins titles aswell as the rest.",
+        playerImg:"images/kane.png"
+    },
+    {
+        player:"Mohamed Salah",
+        nation:"Egypt",
+        ranking:6,
+        position:"Right wing / Striker",
+        team:"Liverpool",
+        age: 29,
+        playerInfo:"Salah wil go down as the best value for a signing ever. Liverpool have turned him to one of the world best players in a short time. Salah is just class, he was just 2 goals away from breaking the all-time Premier League record for a single season. Salah is the best player that have ever come from Egypt",
+        playerImg:"images/salah.png"
+    },
+    {
+        player:"Kylian Mbappe",
+        nation:"France",
+        ranking:7,
+        position:"Striker",
+        team:"PSG",
+        age: 22,
+        playerInfo:"Mbappe is a future no.1 football player. He is so talented and have the potential to be one of the greatest. This is probably his last season at PSG, the star wants a new adventure in a new club. Lets hope the MNK trio delivers this years in the CL",
+        playerImg:"images/mbappe.png"
+    },
+    {
+        player:"Neymar",
+        nation:"Brazil",
+        ranking:8,
+        position:"Left wing",
+        team:"PSG",
+        age: 29,
+        playerInfo:"Neymar is one of the greatest ballers we have seen this century. Neymar have won the CL with Barcelona, but have yet to win with PSG. Thats why he is ranked no.8 he has yet to win the most wanted trophy in football with PSG. Many people say that he have lost his skills since leaving Barcelona.",
+        playerImg:"images/neymar.png"
+    },
+    {
+        player:"Erling Haaland",
+        nation:"Norway",
+        ranking:9,
+        position:"Striker",
+        team:"Dortmund",
+        age: 21,
+        playerInfo:"The most exciting player this century has ever seen. A norwegian as a world class player, who would thought. With the most amazing stat of scoring 49 goals in just 49 games for Dortmund is just amazing. Haaland has the potential to be one of the best. ",
+        playerImg:"images/haaland.png"
+    },
+    {
+        player:"Virgil van Dijk",
+        nation:"Holland",
+        ranking:10,
+        position:"Defender",
+        team:"Liverpool",
+        age: 30,
+        playerInfo:"van Dijk is probably the best defender in the world right now. He have won the Premier League and CL with Liverpool, and lots of the honor goes to him. He have helped Liverpool to become one of the best teams in the world right now",
+        playerImg:"images/vandijk.png"
     }
 ]
 
