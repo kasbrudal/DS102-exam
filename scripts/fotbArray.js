789bab186ebd8eeeb09c12e617e06db2b00c5e36
Kasper <peephyt@gmail.com>
2021-11-08 13:31:07 +0100

    as

---
+++ scripts/fotbArray.js
@@ -0,0 +1,14 @@
+var fotbArray = [
+    {
+       player:"Lionel Messi",
+       ranking:1,
+       position:"Right wing",
+       team:"PSG",
+       age: 34,
+       playerInfo:"",
+       playerImg
+
+
+    }
+]
+
